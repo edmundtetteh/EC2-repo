e625f416f3f9a50fd982494fe7e0faf8c3196e67
edmundtetteh <edmundtetteh2000@hotmail.com>
2022-08-08 15:17:34 -0600

    second

--- installdocker.sh
+++ installdocker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -ex
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
+sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chkconfig docker on
+sudo yum install -y git
+sudo curl -l https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl enable docker
